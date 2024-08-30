75a57e34a2cba97d3ba421ed202c2b65f809df11
Philip Meulengracht <philip.meulengracht@canonical.com>
2024-08-30 15:07:07 +0200

    slices: do not add the python3 symlink, do it in test instead

--- tests/spread/integration/netplan.io/test.sh
+++ tests/spread/integration/netplan.io/test.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# create python3 symlink
+ln -s /usr/bin/python3.12 /usr/bin/python3 
+
 # smoketest a couple of netplan commands
 netplan --help | grep "Network configuration in YAML"
 netplan info | grep "features"
