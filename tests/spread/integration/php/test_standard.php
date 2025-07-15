799c346ffd4986376d94a1613562625945e2f0d9
Alex Santisteban <alex.santisteban@canonical.com>
2025-07-15 18:03:55 +0200

    fix: added modules to core and separated other functionality

--- tests/spread/integration/php/test_standard.php
+++ tests/spread/integration/php/test_standard.php
@@ -1,4 +1,11 @@
 <?php
-    $days = cal_days_in_month(0, 2, 2024); // February 2024 (leap year)
-    echo "February 2024 has $days days.";
-?>
+$pharFile = 'test.phar';
+
+try {
+    $phar = new Phar($pharFile);
+    foreach (new RecursiveIteratorIterator($phar) as $file) {
+        echo $file->getPathname() . "\n";
+    }
+} catch (Exception $e) {
+    echo "Failed to open PHAR: " . $e->getMessage();
+}
