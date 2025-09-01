34b652c4dff400b13445187ef3badcf1c76b39f4
marcin <marcin.konowalczyk@canonical.com>
2025-09-01 17:41:07 +0100

    feat: squashed cargo-1.85 from 25.10

---
+++ tests/spread/integration/cargo-1.85/test_hello_crate.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+# spellchecker: ignore rootfs rustc
+
+arch=$(uname -m)
+case "${arch}" in
+    aarch64) chisel_arch="arm64" ;;
+    x86_64) chisel_arch="amd64" ;;
+    *) echo "Unsupported architecture: ${arch}"; exit 1 ;;
+esac
+
+rootfs="$(install-slices --arch "$chisel_arch" cargo-1.85_cargo)"
+ln -s gcc "$rootfs/usr/bin/cc"
+ln -s rustc-1.85 "$rootfs/usr/bin/rustc"
+
+# Create minimal /dev/null 
+mkdir -p "$rootfs/dev"
+touch "$rootfs/dev/null"
+chmod +x "$rootfs/dev/null"
+
+cp -r testfiles/hello_crate "$rootfs"
+
+chroot "$rootfs" cargo-1.85 -Z unstable-options -C /hello_crate build --workspace
+chroot "$rootfs" ./hello_crate/target/debug/hello | grep -q "Hello, world!"
