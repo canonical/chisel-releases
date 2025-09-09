4d0b76e061890fff6dea3899a5579d2bd543b763
marcin <marcin.konowalczyk@canonical.com>
2025-09-09 12:27:03 +0100

    feat: update rustc-1.84 spread tests

---
+++ tests/spread/integration/rustc-1.84/test_std.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+if [[ "$1" != "--spread" ]]; then
+    FILE_DIR=$(realpath "$(dirname "$0")")
+    source "$FILE_DIR"/setup.sh
+fi
+
+## TESTS 
+# spellchecker: ignore rootfs rustc libgcc
+
+rootfs="$(install-slices rustc-1.84_rustc)"
+arch=$(uname -m)-linux-gnu
+slices=(
+    rustc-1.84_rustc
+    libgcc-14-dev_libs
+    gcc-14-"$(echo "$arch" | sed 's/_/-/')"_rustc-184-minimal
+)
+rootfs="$(install-slices "${slices[@]}")"
+ln -s "${arch}"-gcc-14 "${rootfs}"/usr/bin/cc
+ln -s "${arch}"-ld "${rootfs}"/usr/bin/ld
+
+cp test_std.rs "${rootfs}"/test_std.rs
+chroot "${rootfs}" rustc-1.84 /test_std.rs -o /test_std
+chroot "${rootfs}" /test_std
