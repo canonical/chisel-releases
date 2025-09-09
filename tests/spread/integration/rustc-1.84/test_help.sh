d88fa32e38efc70c0fd03b94c653e25ceb951cc9
marcin <marcin.konowalczyk@canonical.com>
2025-09-09 12:27:03 +0100

    feat: update rustc-1.84 spread tests

---
+++ tests/spread/integration/rustc-1.84/test_help.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+if [[ "$1" != "--spread" ]]; then
+    FILE_DIR=$(realpath "$(dirname "$0")")
+    source "$FILE_DIR"/setup.sh
+fi
+
+## TESTS 
+# spellchecker: ignore rootfs rustc
+
+rootfs="$(install-slices rustc-1.84_rustc)"
+chroot "${rootfs}/" rustc-1.84 --help | grep -q "Usage: rustc"
