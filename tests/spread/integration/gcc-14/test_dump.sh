6ba2bf46c431d788c0ea7575b94bc6bc1602615a
marcin <marcin.konowalczyk@canonical.com>
2025-09-12 21:44:49 +0100

    feat: gcc-14 slices

---
+++ tests/spread/integration/gcc-14/test_dump.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+if [[ "$1" != "--spread" ]]; then
+    FILE_DIR=$(realpath "$(dirname "$0")")
+    source "$FILE_DIR"/setup.sh
+fi
+
+## TESTS 
+# spellchecker: ignore rootfs dumpmachine dumpversion dumpspecs
+
+arch=$(uname -m)-linux-gnu
+rootfs="$(install-slices gcc-14-"${arch//_/-/}"_gcc-14)"
+ln -s "${arch}-gcc-14" "${rootfs}/usr/bin/gcc"
+
+dumpmachine=$(chroot "${rootfs}" gcc -dumpmachine)
+test "$dumpmachine" = "$arch"
+dumpversion=$(chroot "${rootfs}" gcc -dumpversion)
+test "$dumpversion" = "14"
+
+# shellcheck disable=SC2063
+dumpspecs=$(chroot "${rootfs}" gcc -dumpspecs | grep '^*' | tr '\n' ' ')
+expected_keys=("asm" "cc1" "cpp" "link" "lib")
+for key in "${expected_keys[@]}"; do
+    # shellcheck disable=SC2063
+    echo "$dumpspecs" | grep -q "*${key}:"
+done
---
+++ tests/spread/integration/gcc-14/test_hello.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+if [[ "$1" != "--spread" ]]; then
+    FILE_DIR=$(realpath "$(dirname "$0")")
+    source "$FILE_DIR"/setup.sh
+fi
+
+## TESTS 
+# spellchecker: ignore rootfs binutils
+
+arch=$(uname -m)-linux-gnu
+slices=(
+    gcc-14-"${arch//_/-/}"_gcc-14
+    cpp-14-"${arch//_/-/}"_cc1
+    binutils-"${arch//_/-/}"_assembler
+    binutils-"${arch//_/-/}"_linker
+    libgcc-14-dev_libgcc
+    libc6-dev_posix-libs
+)
+rootfs="$(install-slices "${slices[@]}")"
+ln -s "${arch}-gcc-14" "${rootfs}/usr/bin/gcc"
+ln -s "${arch}-as" "${rootfs}/usr/bin/as"
+ln -s "${arch}-ld" "${rootfs}/usr/bin/ld"
+
+echo '#include <stdio.h>
+int main() {
+    printf("Hello from C!\n");
+    return 0;
+}' > ${rootfs}/hello.c
+chroot "${rootfs}" gcc /hello.c -o /hello
+chroot "${rootfs}" /hello | grep -q "Hello from C!"
