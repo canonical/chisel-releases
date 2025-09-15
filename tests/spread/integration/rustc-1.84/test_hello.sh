807c74b7aa6b43d2749f33a4968b202620c8b483
marcin <marcin.konowalczyk@canonical.com>
2025-09-15 14:34:06 +0100

    feat(testing): update test slice names

--- tests/spread/integration/rustc-1.84/test_hello.sh
+++ tests/spread/integration/rustc-1.84/test_hello.sh
@@ -6,19 +6,20 @@ if [[ "$1" != "--spread" ]]; then
 fi
 
 ## TESTS 
-# spellchecker: ignore rootfs rustc libgcc
+# spellchecker: ignore rootfs rustc binutils libgcc println
 
 rootfs="$(install-slices rustc-1.84_rustc)"
 arch=$(uname -m)-linux-gnu
 slices=(
     rustc-1.84_rustc
-    libgcc-14-dev_libs
-    gcc-14-"$(echo "$arch" | sed 's/_/-/')"_rustc-184-minimal
+    gcc-14-"${arch//_/-}"_gcc-14
+    binutils-"${arch//_/-}"_linker
+    libgcc-14-dev_libgcc
 )
 rootfs="$(install-slices "${slices[@]}")"
 ln -s "${arch}"-gcc-14 "${rootfs}"/usr/bin/cc
 ln -s "${arch}"-ld "${rootfs}"/usr/bin/ld
 
-echo 'fn main() { println!("Hello from Rust!"); }' > ${rootfs}/hello.rs
+echo 'fn main() { println!("Hello from Rust!"); }' > "${rootfs}"/hello.rs
 chroot "${rootfs}" rustc-1.84 /hello.rs -o /hello
 chroot "${rootfs}" /hello | grep -q "Hello from Rust!"
--- tests/spread/integration/rustc-1.84/test_help_and_version.sh
+++ tests/spread/integration/rustc-1.84/test_help_and_version.sh
@@ -10,3 +10,4 @@ fi
 
 rootfs="$(install-slices rustc-1.84_rustc)"
 chroot "${rootfs}/" rustc-1.84 --help | grep -q "Usage: rustc"
+chroot "${rootfs}/" rustc-1.84 --version | grep -q 'rustc 1.84'
