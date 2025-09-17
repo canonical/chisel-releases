20517eb48b562d038ee2c530b85f22df6e8c3462
marcin <marcin.konowalczyk@canonical.com>
2025-09-17 09:15:59 +0100

    fea(apt): lua test

---
+++ tests/spread/integration/apt/test_install_lua.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+if [[ "$1" != "--spread" ]]; then
+    FILE_DIR=$(realpath "$(dirname "$0")")
+    source "$FILE_DIR"/setup.sh
+fi
+
+## TESTS 
+# spellchecker: ignore rootfs resolv localtime postinst tzdata zoneinfo
+rootfs="$(install-slices apt_apt-get)"
+
+# For DNS resolution
+cp /etc/resolv.conf "$rootfs/etc/resolv.conf"
+
+chroot "$rootfs" apt update
+chroot "$rootfs" apt install -y lua5.4=
+chroot "$rootfs" lua5.4 -e 'print("Hello, World!")' | grep -q "Hello, World!"
+
