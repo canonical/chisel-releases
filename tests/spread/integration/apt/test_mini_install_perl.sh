6ea6bbdf1d56bb76ed67b59f50764f311249050a
marcin <marcin.konowalczyk@canonical.com>
2025-09-17 21:11:24 +0100

    feat(apt): test mini perl

---
+++ tests/spread/integration/apt/test_mini_install_perl.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+if [[ "$1" != "--spread" ]]; then
+    FILE_DIR=$(realpath "$(dirname "$0")")
+    source "$FILE_DIR"/setup.sh
+else
+    apt update
+fi
+
+## TESTS 
+# spellchecker: ignore rootfs resolv coreutils
+rootfs="$(install-slices apt_apt-get-mini)"
+
+# For DNS resolution
+cp /etc/resolv.conf "$rootfs/etc/resolv.conf"
+
+# Perl wants /dev/null
+mkdir -p "$rootfs/dev"
+touch "$rootfs/dev/null"
+chmod +x "$rootfs/dev/null"
+
+chroot "$rootfs" apt update
+chroot "$rootfs" apt install -y --no-install-recommends coreutils dpkg apt # bootstrap apt with itself
+chroot "$rootfs" apt install -y perl
+chroot "$rootfs" perl -e 'print "Hello, World!\n"' | grep -q "Hello, World!"
