37dd4ec0777b849da02ad1edf6a15978a1374360
marcin <marcin.konowalczyk@canonical.com>
2025-09-17 09:16:05 +0100

    fea(apt): perl test

---
+++ tests/spread/integration/apt/test_install_perl.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+if [[ "$1" != "--spread" ]]; then
+    FILE_DIR=$(realpath "$(dirname "$0")")
+    source "$FILE_DIR"/setup.sh
+fi
+
+## TESTS 
+# spellchecker: ignore rootfs resolv localtime postinst tzdata zoneinfo
+rootfs="$(install-slices apt_apt-get)"
+
+# For DNS resolution
+cp /etc/resolv.conf "$rootfs/etc/resolv.conf"
+
+# Perl wants /dev/null
+mkdir -p "$rootfs/dev"
+touch "$rootfs/dev/null"
+chmod +x "$rootfs/dev/null"
+
+chroot "$rootfs" apt update
+chroot "$rootfs" apt install -y perl
+chroot "$rootfs" perl -e 'print "Hello, World!\n"' | grep -q "Hello, World!"
+
