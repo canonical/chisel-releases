99fc988c80465e7905654970a1d2e822c7233e45
marcin <marcin.konowalczyk@canonical.com>
2025-09-17 09:15:53 +0100

    fea(apt): simplify python test

--- tests/spread/integration/apt/test_install_python.sh
+++ tests/spread/integration/apt/test_install_python.sh
@@ -9,25 +9,15 @@ fi
 # spellchecker: ignore rootfs resolv localtime postinst tzdata zoneinfo
 rootfs="$(install-slices apt_apt-get)"
 
-mkdir -p "$rootfs/dev"
-touch "$rootfs/dev/null"
-chmod +x "$rootfs/dev/null"
 # For DNS resolution
 cp /etc/resolv.conf "$rootfs/etc/resolv.conf"
 
+# Python wants /dev/null
+mkdir -p "$rootfs/dev"
+touch "$rootfs/dev/null"
+chmod +x "$rootfs/dev/null"
+
 chroot "$rootfs" apt update
-
-# Install tzdata in a non-interactive mode. This is a python dependency
-mkdir -p "$rootfs/usr/share/zoneinfo/Etc/"
-cp /usr/share/zoneinfo/Etc/UTC "$rootfs/usr/share/zoneinfo/Etc/UTC"
-ln -s /usr/share/zoneinfo/Etc/UTC "$rootfs/etc/localtime"
-chroot "$rootfs" apt install -y tzdata
-
-# INstall python3.13 postinst dependencies
-chroot "$rootfs" apt install -y grep
-
-# Install python3.13
 chroot "$rootfs" apt install python3.13 -y
-
-chroot "${rootfs}/" python3.13 -c 'print("Hello, World!")' | grep -q "Hello, World!"
+chroot "$rootfs" python3.13 -c 'print("Hello, World!")' | grep -q "Hello, World!"
 
