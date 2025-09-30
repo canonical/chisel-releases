f098011411c1c42a18d432e6a0f8279a9b81aa0d
marcin <marcin.konowalczyk@canonical.com>
2025-09-30 09:48:15 +0100

    doc: add note to determine_ignore_flag

--- .github/scripts/install-slices/version-matrix.py
+++ .github/scripts/install-slices/version-matrix.py
@@ -3,6 +3,10 @@
 import os
 import json
 
+# NOTE: This is a temporary function to determine the ignore flag which we then
+#       pass to install-slices.py. We will likely refactor this in the future
+#       to be a neater and the logic will be moved away from here.
+#       See: https://github.com/canonical/chisel-releases/issues/675
 def determine_ignore_flag(ignore: str, chisel_version: str) -> str:
     ignore_flag = ""
     if chisel_version == "main":
