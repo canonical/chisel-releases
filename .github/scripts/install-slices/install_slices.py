38768fe5a97c63718a41c9fa3731503ff140ee9d
Marcin Konowalczyk <marcin.konowalczyk@canonical.com>
2025-10-02 08:00:59 +0100

    ci: ignore unstable chisel releases (#683)

--- .github/scripts/install-slices/install_slices.py
+++ .github/scripts/install-slices/install_slices.py
@@ -285,7 +285,7 @@ def ignore_missing_packages(
 
 
 def install_slices(
-    chunk: list[tuple[str, str]], dry_run: bool, arch: str, release: str, worker: int
+    chunk: list[tuple[str, str]], dry_run: bool, arch: str, release: str, worker: int, extra_flag: str
 ) -> None:
     """
     Install the slice by running "chisel cut".
@@ -315,6 +315,7 @@ def install_slices(
                     release,
                     "--root",
                     tmpfs,
+                    extra_flag,
                     slice_name,
                 ],
                 capture_output=True,
@@ -410,6 +411,7 @@ def main() -> None:
         logging.info("No slices will be installed.")
         return
 
+    extra_flag = '--ignore=unstable' if subprocess.check_output(["chisel","version"]).strip().decode().lstrip("v") > "1.2.0" else ''
     # TODO: this list of slices can still be reduced, since many of these
     # will come as essentials of other slices. We could simply crawl all essentials
     # in the release, and remove them from the "all_slices" list (since they
@@ -426,6 +428,7 @@ def main() -> None:
             cli_args.arch,
             cli_args.release,
             i // chunk_size + 1,  # worker number
+            extra_flag
         )
         for i in range(0, len(all_slices), chunk_size)
     ]
