c3d830d170339183d658ebca9b340a06dc49f7c8
lczyk <marcin.konowalczyk@canonical.com>
2025-10-22 23:30:18 +0100

    feat!: test cross preproc

--- tests/spread/integration/cpp-14-aarch64-linux-gnu/test_preprocessor.sh
+++ tests/spread/integration/cpp-14-aarch64-linux-gnu/test_preprocessor.sh
@@ -27,15 +27,11 @@ cp question.c "${rootfs}/question.c"
 mkdir -p "${rootfs}/usr/include/everything"
 cp answer.h "${rootfs}/usr/include/everything/"
 
-if $cross; then
-    # TODO: We do not have libc6-dev for cross /usr/lib64/ld-linux-x86-64
-    :
-else
-    chroot "${rootfs}" cc1 -E question.c > "${rootfs}/question.i" 2>/dev/null
-    cat "${rootfs}/question.i" | grep -q 'return 42;'
 
-    # now remove answer.h and check that ANSWER is not defined
-    echo "" > "${rootfs}/usr/include/everything/answer.h"
-    chroot "${rootfs}" cc1 -E question.c > "${rootfs}/question.i" 2>/dev/null
-    cat "${rootfs}/question.i" | grep -q 'return 1;'
-fi
+chroot "${rootfs}" cc1 -E question.c > "${rootfs}/question.i" 2>/dev/null
+cat "${rootfs}/question.i" | grep -q 'return 42;'
+
+# now remove answer.h and check that ANSWER is not defined
+echo "" > "${rootfs}/usr/include/everything/answer.h"
+chroot "${rootfs}" cc1 -E question.c > "${rootfs}/question.i" 2>/dev/null
+cat "${rootfs}/question.i" | grep -q 'return 1;'
