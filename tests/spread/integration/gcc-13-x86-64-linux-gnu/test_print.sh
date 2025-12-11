1821ead40fdb5562a9f0a13f14a9d1a51af96b00
Zhijie Yang <zhijie.yang@canonical.com>
2025-12-11 23:16:10 +0100

    chore(24.04): adjust comments and tests

--- tests/spread/integration/gcc-13-x86-64-linux-gnu/test_print.sh
+++ tests/spread/integration/gcc-13-x86-64-linux-gnu/test_print.sh
@@ -26,7 +26,7 @@ rootfs="$(install-slices gcc-13-x86-64-linux-gnu_gcc-13)"
 test "$(chroot "${rootfs}" gcc -print-search-dirs | head -n 1)" = "install: /usr/lib/$gcc_dir/x86_64-linux-gnu/13/"
 chroot "${rootfs}" gcc -print-search-dirs | head -n 2 | tail -n 1 | grep -q "/usr/libexec/$gcc_dir/x86_64-linux-gnu/13/"
 
-test "$(chroot "${rootfs}" gcc -print-libgcc-file-name)" = "libgcc.a"
+test "$(chroot "${rootfs}" gcc -print-libgcc-file-name)" = "/usr/lib/$gcc_dir/x86_64-linux-gnu/13/libgcc.a"
 chroot "${rootfs}" gcc -print-file-name=libc.so.6 | grep -q "libc.so.6"
 
 # create a fake program called 'foo' in libexec dir to test -print-prog-name
