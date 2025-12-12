5832bc059ae527b9f6f78b684bb87800a6de0d2a
lczyk <marcin.konowalczyk@canonical.com>
2025-12-12 13:46:13 +0000

    feat!: rename to 1.75

--- tests/spread/integration/rustc-1.75/test_std.sh
+++ tests/spread/integration/rustc-1.75/test_std.sh
@@ -1,21 +1,21 @@
 #!/usr/bin/env bash
-# spellchecker: ignore rootfs rustc binutils libgcc println
+# spellchecker: ignore rootfs rustc binutils libgcc
 
 # NOTE: We do not have the arch-independent slices for gcc/binutils yet,
 #       so we need to determine the architecture here.
 #       See: https://github.com/canonical/chisel-releases/issues/761
 arch=$(uname -m)-linux-gnu
 slices=(
-    rustc-1.84_rustc
-    gcc-14-"${arch//_/-}"_gcc-14
+    rustc-1.75_rustc
+    gcc-13-"${arch//_/-}"_gcc-13
     binutils-"${arch//_/-}"_linker
-    libgcc-14-dev_core
+    libgcc-13-dev_core
 )
 rootfs="$(install-slices "${slices[@]}")"
-ln -s "${arch}"-gcc-14 "${rootfs}"/usr/bin/cc
+ln -s "${arch}"-gcc-13 "${rootfs}"/usr/bin/cc
 ln -s "${arch}"-ld "${rootfs}"/usr/bin/ld
 
-cp testfiles/hello.rs "${rootfs}/hello.rs"
+cp testfiles/test_std.rs "${rootfs}"/test_std.rs
 
-chroot "${rootfs}" rustc-1.84 /hello.rs -o /hello
-chroot "${rootfs}" /hello | grep -q "Hello from Rust!"
+chroot "${rootfs}" rustc-1.75 /test_std.rs -o /test_std
+chroot "${rootfs}" /test_std
--- tests/spread/integration/rustc-1.77/test_help_and_version.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-# spellchecker: ignore rootfs rustc
-
-rootfs="$(install-slices rustc-1.84_rustc)"
-chroot "${rootfs}/" rustc-1.84 --help | grep -q "Usage: rustc"
-chroot "${rootfs}/" rustc-1.84 --version | grep -q 'rustc 1.84'
