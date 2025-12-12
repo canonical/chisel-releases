f617132e8cd41b5bdbb3df45d35137372095f35d
lczyk <marcin.konowalczyk@canonical.com>
2025-12-12 13:35:23 +0000

    feat!: copy rust 1.84

---
+++ tests/spread/integration/rustc-1.77/test_std.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+# spellchecker: ignore rootfs rustc binutils libgcc
+
+# NOTE: We do not have the arch-independent slices for gcc/binutils yet,
+#       so we need to determine the architecture here.
+#       See: https://github.com/canonical/chisel-releases/issues/761
+arch=$(uname -m)-linux-gnu
+slices=(
+    rustc-1.84_rustc
+    gcc-14-"${arch//_/-}"_gcc-14
+    binutils-"${arch//_/-}"_linker
+    libgcc-14-dev_core
+)
+rootfs="$(install-slices "${slices[@]}")"
+ln -s "${arch}"-gcc-14 "${rootfs}"/usr/bin/cc
+ln -s "${arch}"-ld "${rootfs}"/usr/bin/ld
+
+cp testfiles/test_std.rs "${rootfs}"/test_std.rs
+
+chroot "${rootfs}" rustc-1.84 /test_std.rs -o /test_std
+chroot "${rootfs}" /test_std
