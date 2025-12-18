a4dc321533064c516499346c258e3f49778b3962
lczyk <marcin.konowalczyk@canonical.com>
2025-12-18 09:03:49 +0000

    feat(25.10): archiver slice

--- tests/spread/integration/binutils/test_archiver.sh
+++ tests/spread/integration/binutils/test_archiver.sh
@@ -12,8 +12,9 @@ echo "Unsupported architecture: ${arch}"
 exit 1
 fi
 
-rootfs="$(install-slices --arch "${chisel_arch}" binutils_assembler binutils_linker)"
+rootfs="$(install-slices --arch "${chisel_arch}" binutils_archiver)"
 
-chroot "${rootfs}/" as --version | grep "GNU assembler"
-chroot "${rootfs}/" ld --version | grep "GNU ld"
-chroot "${rootfs}/" ld.bfd --version | grep "GNU ld"
+touch "$rootfs/file1" "$rootfs/file2"
+chroot "$rootfs" ar rcs archive file1 file2
+chroot "$rootfs" ar t archive | grep -q "file1"
+chroot "$rootfs" ar t archive | grep -q "file2"
