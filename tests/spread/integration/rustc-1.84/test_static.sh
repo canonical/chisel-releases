6d10c761ea08f6454463d6699ce326fbbad77783
lczyk <marcin.konowalczyk@canonical.com>
2026-01-12 17:23:44 +0000

    Squashed commit of the following:
    commit 1a3ba8b02c41dbc1efec2e7ae7c8dcc41d3a9d17
    Merge: 6072639 a520c7b
    Author: Marcin Konowalczyk <marcin.konowalczyk@canonical.com>
    Date:   Wed Jan 7 17:30:25 2026 +0000
    
        Merge branch 'ubuntu-25.04' into ROCKS-2271/rust-1.84-25.04
    
    commit 60726396dfd1015a8ce4be665e4b1cbb1b267b57
    Author: lczyk <marcin.konowalczyk@canonical.com>
    Date:   Mon Dec 15 09:24:31 2025 +0000
    
        feat: rustc slices
    
    commit 6b39afb02b3cea68d37b4187750029b27ec98fdd
    Author: lczyk <marcin.konowalczyk@canonical.com>
    Date:   Mon Dec 15 09:07:22 2025 +0000
    
        feat: refactor based on 1.75
    
    commit 1e56af99a2078435302f69fd1f8a3cbaeafec806
    Author: lczyk <marcin.konowalczyk@canonical.com>
    Date:   Wed Dec 10 10:36:39 2025 +0000
    
        feat: removed gcc-15-base
    
    commit 94f2d6313913fc4ded39197aacf17e3f6082ed1e
    Author: lczyk <marcin.konowalczyk@canonical.com>
    Date:   Mon Dec 8 12:51:30 2025 +0000
    
        fix: typo
    
    commit 386beeb1453c9ddbb0f56d33c7e7c07989b545d2
    Author: lczyk <marcin.konowalczyk@canonical.com>
    Date:   Mon Dec 8 12:48:09 2025 +0000
    
        docs: more arch notes
    
    commit 2b5069eb7395924f493303d872ae554d2ac6a1c5
    Author: lczyk <marcin.konowalczyk@canonical.com>
    Date:   Mon Dec 8 12:47:35 2025 +0000
    
        docs: arch note
    
    commit fd7d60ee94c1173f9bdb6c08b2305ba500e5b136
    Author: lczyk <marcin.konowalczyk@canonical.com>
    Date:   Mon Dec 8 12:43:09 2025 +0000
    
        feat: rearrange tests
    
    commit 847f914a4e90bb26b2ee470ae2be454edf1a9b77
    Author: marcin <marcin.konowalczyk@canonical.com>
    Date:   Mon Sep 22 18:04:44 2025 +0100
    
        feat: minor linting nitpicks
    
    commit abe7a96b2424855122d5afa59ce7162af7846298
    Author: marcin <marcin.konowalczyk@canonical.com>
    Date:   Mon Sep 22 18:00:29 2025 +0100
    
        doc: update slice doc
    
    commit 5969666197d83a42d3af5eea6b29310f0d7b1af4
    Author: marcin <marcin.konowalczyk@canonical.com>
    Date:   Mon Sep 22 18:00:19 2025 +0100
    
        feat: remove needless installs in tests
    
    commit 7d18d4bdb6e974a76b2cf9ec367b9d09d0701672
    Author: marcin <marcin.konowalczyk@canonical.com>
    Date:   Mon Sep 22 17:37:22 2025 +0100
    
        feat: update libc6-dev and libgcc-14-dev refs
    
    commit 0aff37be88356826cb20f61f03675830f216b720
    Author: marcin <marcin.konowalczyk@canonical.com>
    Date:   Tue Sep 16 11:34:19 2025 +0100
    
        chore: move copyright slice to the end
    
    commit d05f0b9bea9ea3d8065c473cdb23b62dca63802c
    Author: marcin <marcin.konowalczyk@canonical.com>
    Date:   Tue Sep 16 10:48:12 2025 +0100
    
        feat: newlines at the ends of files
    
    commit 291e383368c37cf620db0e9ccfb07d005411f408
    Author: marcin <marcin.konowalczyk@canonical.com>
    Date:   Tue Sep 16 10:47:04 2025 +0100
    
        feat: nicer sudo chroot patch
    
    commit 31ce96678332285ac23fad94d882bc5ea79a6d33
    Author: marcin <marcin.konowalczyk@canonical.com>
    Date:   Tue Sep 16 08:49:13 2025 +0100
    
        chore: relative symlinks in docs
    
    commit 807c74b7aa6b43d2749f33a4968b202620c8b483
    Author: marcin <marcin.konowalczyk@canonical.com>
    Date:   Mon Sep 15 14:34:06 2025 +0100
    
        feat(testing): update test slice names
    
    commit 6408fb1e3a0787b3e94512f021a5f35f9f55f2f6
    Author: marcin <marcin.konowalczyk@canonical.com>
    Date:   Wed Sep 10 11:09:10 2025 +0100
    
        feat: update libc6-dev slices
    
    commit d88fa32e38efc70c0fd03b94c653e25ceb951cc9
    Author: marcin <marcin.konowalczyk@canonical.com>
    Date:   Tue Sep 9 12:27:03 2025 +0100
    
        feat: update rustc-1.84 spread tests
    
    commit a789afe6a113f8f70814e69fa33f50280c36060e
    Author: marcin <marcin.konowalczyk@canonical.com>
    Date:   Tue Sep 9 12:26:30 2025 +0100
    
        fix: rust-1.84 essential slices
    
    commit 4eb10698fdec982cdfea59b77ff43cfb705fa041
    Author: marcin <marcin.konowalczyk@canonical.com>
    Date:   Mon Aug 4 16:17:50 2025 +0100
    
        feat: Squashed rustc-1.84 slices

---
+++ tests/spread/integration/rustc-1.84/test_static.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+# spellchecker: ignore rootfs rustc
+
+arch=$(uname -m)
+case "${arch}" in
+    aarch64) chisel_arch="arm64" ;;
+    x86_64) chisel_arch="amd64" ;;
+    *) echo "Unsupported architecture: ${arch}"; exit 1 ;;
+esac
+
+rootfs="$(install-slices --arch "$chisel_arch" rustc-1.84_rustc)"
+ln -s gcc "$rootfs/usr/bin/cc"
+
+cp testfiles/greeter.rs "$rootfs/greeter.rs"
+cp testfiles/use_greeter.c "$rootfs/use_greeter.c"
+
+chroot "$rootfs" rustc-1.84 /greeter.rs --crate-type staticlib -o /libgreeter.a
+test -f "$rootfs/libgreeter.a"
+
+# Compile and link C program against the static library
+chroot "$rootfs" gcc /use_greeter.c -L/ -lgreeter -o /use_greeter
+chroot "$rootfs" /use_greeter | grep -q "Hello to C from Rust static library!"
