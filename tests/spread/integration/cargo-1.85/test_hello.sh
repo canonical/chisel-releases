64a3f26af44414d0d3a52c1c850e35b0a5c5d983
lczyk <marcin.konowalczyk@canonical.com>
2026-01-13 00:12:06 +0000

    feat: rustc symlink

--- tests/spread/integration/cargo-1.85/test_hello.sh
+++ tests/spread/integration/cargo-1.85/test_hello.sh
@@ -10,6 +10,7 @@ esac
 
 rootfs="$(install-slices --arch "$chisel_arch" cargo-1.85_cargo)"
 ln -s gcc "$rootfs/usr/bin/cc"
+ln -s rustc-1.85 "$rootfs/usr/bin/rustc"
 
 # Create minimal /dev/null 
 mkdir -p "$rootfs/dev"
