e1a1278b94aa465c63fb96536d30647fb0c454f1
lczyk <marcin.konowalczyk@canonical.com>
2026-01-13 14:08:09 +0000

    feat: cargo-1.84 tests

---
+++ tests/spread/integration/cargo-1.84/test_eza.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+# spellchecker: ignore rootfs binutils archiver resolv
+
+arch=$(uname -m)
+case "${arch}" in
+    aarch64) chisel_arch="arm64" ;;
+    x86_64) chisel_arch="amd64" ;;
+    *) echo "Unsupported architecture: ${arch}"; exit 1 ;;
+esac
+
+slices=(
+    cargo-1.84_cargo
+    binutils_archiver # the zlib dependency requires ar
+    ca-certificates_data # for HTTPS access to crates.io
+)
+
+rootfs="$(install-slices --arch "$chisel_arch" "${slices[@]}")"
+ln -s gcc "$rootfs/usr/bin/cc"
+ln -s rustc-1.84 "$rootfs/usr/bin/rustc"
+
+# Create minimal /dev/null 
+mkdir -p "$rootfs/dev" && touch "$rootfs/dev/null" && chmod +x "$rootfs/dev/null"
+
+# We need DNS to fetch crates.io dependencies
+mkdir -p "$rootfs/etc" && cp /etc/resolv.conf "$rootfs/etc/resolv.conf"
+
+# Enable apt source downloads
+# NOTE: we need dpkg-dev to unpack the source
+sed -i 's|^Types:.*|Types: deb deb-src|' /etc/apt/sources.list.d/ubuntu.sources
+apt update && apt install -y dpkg-dev
+
+# Download source
+(
+    cd "$rootfs" || exit 1
+    apt source rust-eza -y
+    mv rust-eza-* rust-eza
+)
+
+# Build
+chroot "$rootfs" cargo-1.84 -Z unstable-options -C /rust-eza build
+
+# Verify the built binary works
+chroot "$rootfs" /rust-eza/target/debug/eza --help | grep -q "eza \[options\] \[files...\]"
+touch "$rootfs/tmp/testfile"
+chroot "$rootfs" /rust-eza/target/debug/eza /tmp | grep -q "testfile"
---
+++ tests/spread/integration/cargo-1.84/test_help_and_version.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+# spellchecker: ignore rootfs
+
+arch=$(uname -m)
+case "${arch}" in
+    aarch64) chisel_arch="arm64" ;;
+    x86_64) chisel_arch="amd64" ;;
+    *) echo "Unsupported architecture: ${arch}"; exit 1 ;;
+esac
+
+rootfs="$(install-slices --arch "$chisel_arch" cargo-1.84_cargo)"
+# ln -s gcc "$rootfs/usr/bin/cc"  # not needed for help/version
+# ln -s rustc-1.84 "$rootfs/usr/bin/rustc"  # not needed for help/version
+
+chroot "$rootfs" cargo-1.84 --help | grep -q "Rust's package manager"
+chroot "$rootfs" cargo-1.84 --version | grep -q 'cargo 1.84'
