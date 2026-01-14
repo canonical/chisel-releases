b299c4a2ff468ded004749fc67131878d2d5f5e4
lczyk <marcin.konowalczyk@canonical.com>
2026-01-14 18:38:20 +0000

    feat!: gcc-$arch symlink

--- tests/spread/integration/golang-1.24-go/test_cgo-tools.sh
+++ tests/spread/integration/golang-1.24-go/test_cgo-tools.sh
@@ -1,3 +1,10 @@
+
+# TODO: go tool gco calls gcc though the symlink is missing in the rootfs
+#       once https://github.com/canonical/chisel-releases/pull/836 is merged,
+#       we can add it to the essentials and remove this workaround
+arch=$(uname -m)
+ln -s "$arch-linux-gnu-gcc-15" "${rootfs}/usr/bin/${arch}-linux-gnu-gcc"
+
 chroot "${rootfs}" /usr/lib/go-1.24/bin/go tool cgo -V
 cp ../golang/hello/cmd/hello_cgo/main_cgo.go "${rootfs}/main_cgo.go"
 chroot "${rootfs}" /usr/lib/go-1.24/bin/go tool cgo main_cgo.go
