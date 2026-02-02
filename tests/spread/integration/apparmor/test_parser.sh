e07ae4f444e56701a06785a280a1783195b29b93
lczyk <marcin.konowalczyk@canonical.com>
2026-02-02 09:09:02 +0000

    feat: refactor

--- tests/spread/integration/apparmor/test_parser.sh
+++ tests/spread/integration/apparmor/test_parser.sh
@@ -1,9 +1,16 @@
 #!/usr/bin/env bash
 # spellchecker: ignore rootfs
 
-rootfs="$(install-slices apparmor_bins)"
+rootfs="$(install-slices apparmor_parser)"
 
 chroot "$rootfs" apparmor_parser --help | grep -q "Usage: apparmor_parser"
+
+# copy over profiles for testing
+rootfs_profiles="$(install-slices apparmor_profiles)"
+mkdir -p "$rootfs/etc/apparmor.d"
+cp "$rootfs_profiles/etc/apparmor.d/Discord" "$rootfs/etc/apparmor.d/Discord"
+cp -r "$rootfs_profiles/etc/apparmor.d/abi" "$rootfs/etc/apparmor.d/"
+cp -r "$rootfs_profiles/etc/apparmor.d/tunables" "$rootfs/etc/apparmor.d/"
 
 chroot "$rootfs" apparmor_parser --preprocess /etc/apparmor.d/Discord
 
