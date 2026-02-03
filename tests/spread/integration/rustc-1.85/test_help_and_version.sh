9ceb1706ffc286cfb222cc585fafc9732418f093
Marcin Konowalczyk <marcin.konowalczyk@canonical.com>
2026-02-03 12:16:38 +0000

    feat(24.04): rustc-1.85 slices (#843)

---
+++ tests/spread/integration/rustc-1.85/test_help_and_version.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+# spellchecker: ignore rootfs rustc
+
+arch=$(uname -m)
+case "${arch}" in
+    aarch64) chisel_arch="arm64" ;;
+    x86_64) chisel_arch="amd64" ;;
+    *) echo "Unsupported architecture: ${arch}"; exit 1 ;;
+esac
+
+rootfs="$(install-slices --arch "$chisel_arch" rustc-1.85_rustc)"
+
+chroot "${rootfs}/" rustc-1.85 --help | grep -q "Usage: rustc"
+chroot "${rootfs}/" rustc-1.85 --version | grep -q 'rustc 1.85'
