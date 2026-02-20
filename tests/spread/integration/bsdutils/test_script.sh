a5b0b6bb77c350267a1b6e50a15deb29d59a3273
lczyk <marcin.konowalczyk@canonical.com>
2026-02-20 22:20:17 +0000

    feat: test script

--- tests/spread/integration/bsdutils/test_script.sh
+++ tests/spread/integration/bsdutils/test_script.sh
@@ -1,9 +1,22 @@
 #!/usr/bin/env bash
-#spellchecker: ignore rootfs bsdutils
+#spellchecker: ignore rootfs bsdutils coreutils
 
 rootfs="$(install-slices bsdutils_script)"
 
-chroot "$rootfs" /usr/bin/script --help
-chroot "$rootfs" /usr/bin/script --version
+chroot "$rootfs" /usr/bin/script --help | grep -q "Usage:"
+chroot "$rootfs" /usr/bin/script --version | grep -q "script from"
 
-exit 99
+rootfs_echo=$(install-slices bsdutils_script dash_bins)
+
+mkdir -p "$rootfs_echo"/dev && mount --rbind /dev "$rootfs_echo"/dev
+trap "umount -l $rootfs_echo/dev || true" EXIT
+
+mkdir -p "$rootfs_echo"/tmp
+
+# test the script command with --log-out and --log-in options, which should work without a terminal and without socket errors
+export SHELL=/usr/bin/sh
+chroot "$rootfs_echo" /usr/bin/script \
+    --log-out /tmp/script.log \
+    --command "printf 'hello script'" | grep -q "hello script"
+
+grep -q "hello script" "$rootfs_echo"/tmp/script.log
