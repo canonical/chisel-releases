fefc44de8a2c7fa9203063652c16cb1f9f391876
lczyk <marcin.konowalczyk@canonical.com>
2026-02-20 19:25:27 +0000

    fix: test too arch specific

--- tests/spread/integration/libperl5.38t64/test_scripts.sh
+++ tests/spread/integration/libperl5.38t64/test_scripts.sh
@@ -6,4 +6,4 @@ rootfs=$(install-slices libperl5.40_scripts)
 ls "$rootfs/usr/bin"
 cpan=$(find "$rootfs/usr/bin" -type f -name 'cpan5.40*' | sed "s|$rootfs||")
 test -n "$cpan"
-chroot "$rootfs" "$cpan" --help 2>&1 | grep -q "Usage: cpan5.40-aarch64-linux-gnu"
+chroot "$rootfs" "$cpan" --help 2>&1 | grep -q "Usage: cpan5.40"
