832badc924481b7c55178dc52eed30062194ff57
lczyk <marcin.konowalczyk@canonical.com>
2026-02-20 21:51:39 +0000

    chore: logger

--- tests/spread/integration/bsdutils/test_logger.sh
+++ tests/spread/integration/bsdutils/test_logger.sh
@@ -3,9 +3,9 @@
 
 rootfs="$(install-slices bsdutils_logger)"
 
-chroot "$rootfs" /usr/bin/logger --help| grep -q "Usage:"
-chroot "$rootfs" /usr/bin/logger --version| grep -q "logger"
+chroot "$rootfs" /usr/bin/logger --help | grep -q "Usage:"
+chroot "$rootfs" /usr/bin/logger --version | grep -q "logger"
 
 chroot "$rootfs" /usr/bin/logger \
-    --stderr "hello world" \
-    --socket-errors=off 2>&1 | grep -q "hello world"
+    --stderr "hello logger" \
+    --socket-errors=off 2>&1 | grep -q "hello logger"
