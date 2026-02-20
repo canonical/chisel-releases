30747c8ee6147c9d3d04dfee03fd953965107d63
lczyk <marcin.konowalczyk@canonical.com>
2026-02-20 22:22:39 +0000

    feat: scriptlive test

--- tests/spread/integration/bsdutils/test_scriptlive.sh
+++ tests/spread/integration/bsdutils/test_scriptlive.sh
@@ -3,7 +3,15 @@
 
 rootfs="$(install-slices bsdutils_scriptlive)"
 
-chroot "$rootfs" /usr/bin/scriptlive --help
-chroot "$rootfs" /usr/bin/scriptlive --version
+chroot "$rootfs" /usr/bin/scriptlive --help | grep -q "Usage:"
+chroot "$rootfs" /usr/bin/scriptlive --version | grep -q "scriptlive from"
 
-exit 99
+rootfs_echo=$(install-slices bsdutils_scriptlive coreutils_echo)
+mkdir -p "$rootfs_echo"/dev && mount --rbind /dev "$rootfs_echo"/dev
+trap "umount -l $rootfs_echo/dev || true" EXIT
+
+export SHELL=/usr/bin/sh
+chroot "$rootfs_echo" /usr/bin/scriptlive \
+    --log-out /tmp/scriptlive.log \
+    --command "printf 'hello scriptlive'" | grep -q "hello scriptlive"
+grep -q "hello scriptlive" "$rootfs_echo"/tmp/scriptlive.log
