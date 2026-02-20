03a9befe0deabe0d4a5b5b7eb2710f8fb9269da3
lczyk <marcin.konowalczyk@canonical.com>
2026-02-20 21:50:20 +0000

    feat: slimmer logger test

--- tests/spread/integration/bsdutils/test_logger.sh
+++ tests/spread/integration/bsdutils/test_logger.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 #spellchecker: ignore rootfs bsdutils rbind
 
-rootfs="$(install-slices bsdutils_bins)"
+rootfs="$(install-slices bsdutils_logger)"
 
 chroot "$rootfs" /usr/bin/logger --help| grep -q "Usage:"
 chroot "$rootfs" /usr/bin/logger --version| grep -q "logger"
