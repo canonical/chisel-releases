6a8ae59aab0830f6f71743532bbc21b8d8eb84f0
lczyk <marcin.konowalczyk@canonical.com>
2026-03-04 14:36:01 +0000

    feat: test ischroot in tests

--- tests/spread/integration/debianutils/test_ischroot.sh
+++ tests/spread/integration/debianutils/test_ischroot.sh
@@ -2,4 +2,9 @@
 # spellchecker: ignore rootfs debianutils ischroot
 
 rootfs="$(install-slices debianutils_ischroot)"
+chroot "$rootfs" ischroot --help | grep -q "Usage: ischroot"
 chroot "$rootfs" ischroot --version | grep -q "Debian ischroot"
+
+chroot "$rootfs" ischroot --default-false && exit 1
+chroot "$rootfs" ischroot --default-true || exit 1
+
