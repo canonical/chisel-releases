43811220c1d926dbd9d7df439f4d06cbca24dda3
lczyk <marcin.konowalczyk@canonical.com>
2026-03-04 14:30:42 +0000

    chore: own nitpicks

--- tests/spread/integration/debianutils/test_savelog.sh
+++ tests/spread/integration/debianutils/test_savelog.sh
@@ -3,32 +3,8 @@
 
 rootfs="$(install-slices debianutils_savelog)"
 
-# Usage: savelog [-m mode] [-u user] [-g group] [-t] [-c cycle] [-p]
-#              [-j] [-C] [-d] [-l] [-r rolldir] [-n] [-q] file ...
-#         -m mode    - chmod log files to mode
-#         -u user    - chown log files to user
-#         -g group   - chgrp log files to group
-#         -c cycle   - save cycle versions of the logfile (default: 7)
-#         -r rolldir - use rolldir instead of . to roll files
-#         -C         - force cleanup of cycled logfiles
-#         -d         - use standard date for rolling
-#         -D         - override date format for -d
-#         -t         - touch file
-#         -l         - don't compress any log files (default: compress)
-#         -p         - preserve mode/user/group of original file
-#         -j         - use bzip2 instead of gzip
-#         -J         - use xz instead of gzip
-#         -1 .. -9   - compression strength or memory usage (default: 9, except for xz)
-#         -x script  - invoke script with rotated log file in $FILE
-#         -n         - do not rotate empty files
-#         -q         - suppress rotation message
-#         file       - log file names
-
-    #   gzip_bins:
-
+# there is no help so we pass an invalid flag to get the usage message
 chroot "$rootfs" savelog --foo 2>&1| grep -q "Usage: savelog"
-
-chroot "$rootfs" savelog -l /var/log/test.log 2>&1 || true
 
 # savelog with compression. this should fail because we do not include gzip by default
 chroot "$rootfs" savelog /var/log/test.log 2>&1 | \
