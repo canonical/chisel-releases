c099f3b23e9c0fe55190df1c171102ff9e32e4b1
lczyk <marcin.konowalczyk@canonical.com>
2026-03-04 16:11:35 +0000

    feat: test installkernel

--- tests/spread/integration/debianutils/test_installkernel.sh
+++ tests/spread/integration/debianutils/test_installkernel.sh
@@ -3,8 +3,27 @@
 
 rootfs="$(install-slices debianutils_installkernel)"
 
-chroot "$rootfs" installkernel --help 2>&1
-chroot "$rootfs" installkernel --version 2>&1
+chroot "$rootfs" installkernel --help 2>&1 | grep -q "Usage: installkernel"
 
-exit 99
+boot="/foo/bar/boot"
+mkdir -p "$rootfs/$boot"
+echo "dummy image" > "$rootfs/image"
+echo "dummy System.map" > "$rootfs/System.map"
 
+# install
+chroot "$rootfs" installkernel 5.10.0-test /image /System.map "$boot"
+ls "$rootfs/$boot"
+test -f "$rootfs/$boot/vmlinuz-5.10.0-test"
+test -f "$rootfs/$boot/System.map-5.10.0-test"
+
+# install the same version. should create .old files
+chroot "$rootfs" installkernel 5.10.0-test /image /System.map "$boot"
+test -f "$rootfs/$boot/vmlinuz-5.10.0-test.old"
+test -f "$rootfs/$boot/System.map-5.10.0-test.old"
+
+# install a new version
+chroot "$rootfs" installkernel 5.11.0-test /image /System.map "$boot"
+test -f "$rootfs/$boot/vmlinuz-5.11.0-test"
+test -f "$rootfs/$boot/System.map-5.11.0-test"
+test -f "$rootfs/$boot/vmlinuz-5.10.0-test.old"
+test -f "$rootfs/$boot/System.map-5.10.0-test.old"
