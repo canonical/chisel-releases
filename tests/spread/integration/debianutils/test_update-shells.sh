80f591e943089079309fccadc0399153d5cf22dd
lczyk <marcin.konowalczyk@canonical.com>
2026-03-04 15:57:05 +0000

    feat: update shells

--- tests/spread/integration/debianutils/test_update-shells.sh
+++ tests/spread/integration/debianutils/test_update-shells.sh
@@ -3,8 +3,15 @@
 
 rootfs="$(install-slices debianutils_update-shells)"
 
-chroot "$rootfs" update-shells --help 2>&1
-chroot "$rootfs" update-shells --version 2>&1
+chroot "$rootfs" update-shells --help 2>&1 | grep -q "usage: /usr/sbin/update-shells"
 
-exit 99
+# update with no new shells in shells.d
+# should detect the realpath of /bin/sh and add it to /etc/shells
+chroot "$rootfs" update-shells --verbose | grep "adding shell /usr/bin/sh"
+grep -q "/bin/sh" "$rootfs/etc/shells"
+grep -q "/usr/bin/sh" "$rootfs/etc/shells"
 
+# create a new shell and update again
+echo "/foo/bar" > "$rootfs/usr/share/debianutils/shells.d/foo-bar"
+chroot "$rootfs" update-shells --verbose | grep "adding shell /foo/bar"
+grep -q "/foo/bar" "$rootfs/etc/shells"
