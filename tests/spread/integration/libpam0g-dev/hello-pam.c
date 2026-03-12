54eb73d7910579b95170e284ca7dd9d1cb258c6e
lczyk <marcin.konowalczyk@canonical.com>
2026-03-12 12:23:12 +0000

    feat: simplify pam test

--- tests/spread/integration/libpam0g-dev/hello-pam.c
+++ tests/spread/integration/libpam0g-dev/hello-pam.c
@@ -1,24 +1,10 @@
 #include <stdio.h>
 #include <stddef.h>
 #include <security/pam_appl.h>
-#include <security/pam_modules.h>
-
-static int conversation(int num_msg, const struct pam_message **msg,
-                        struct pam_response **resp, void *appdata_ptr) {
-    (void)num_msg;
-    (void)msg;
-    (void)resp;
-    (void)appdata_ptr;
-
-    return PAM_CONV_ERR;
-}
 
 int main(void) {
     pam_handle_t *handle = NULL;
-    struct pam_conv conv = {
-        .conv = conversation,
-        .appdata_ptr = NULL,
-    };
+    struct pam_conv conv = {};
     int status = pam_start("hello-pam", "user", &conv, &handle);
     if (status != PAM_SUCCESS) {
         fprintf(stderr, "PAM error: %s: %s\n", "pam_start", pam_strerror(handle, status));
