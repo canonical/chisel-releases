7b7269a7f8b08b55df69bd0f5d5d3fa2b268d42d
lczyk <marcin.konowalczyk@canonical.com>
2026-03-14 00:52:30 +0000

    feat: procps tests

--- tests/spread/integration/procps/test_bins.sh
+++ tests/spread/integration/procps/test_bins.sh
@@ -6,6 +6,11 @@
 
 rootfs="$(install-slices procps_bins)"
 
+# mount proc for ps and top tests
+mkdir -p "$rootfs"/proc
+mount --bind /proc "$rootfs"/proc
+trap "umount '$rootfs'/proc" EXIT
+
 bins=(
     free
     kill
@@ -23,20 +28,11 @@ bins=(
     vmstat
     w
     watch
+    ps
+    top
 )
 
 for bin in "${bins[@]}"; do
     chroot "$rootfs" "$bin" --help 2>&1 | grep -iq 'usage:'
     chroot "$rootfs" "$bin" --version 2>&1 | grep -q "$bin from procps-ng"
 done
-
-# mount proc for ps and top tests
-mkdir -p "$rootfs"/proc
-mount --bind /proc "$rootfs"/proc
-trap "umount '$rootfs'/proc" EXIT
-
-# top should now work that /proc is mounted
-chroot "$rootfs" top --help 2>&1 | grep -iq 'usage:'
-chroot "$rootfs" top --version 2>&1 | grep -q 'top from procps-ng'
-
-chroot "$rootfs" ps aux 2>&1 | grep -q 'PID'
