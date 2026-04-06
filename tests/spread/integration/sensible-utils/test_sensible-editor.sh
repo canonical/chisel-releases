fbe8c48eab0c81ccc2bf807d62a3f4e2bb6b406c
lczyk <marcin.konowalczyk@canonical.com>
2026-04-06 20:07:13 +0100

    feat: more tests

--- tests/spread/integration/sensible-utils/test_sensible-editor.sh
+++ tests/spread/integration/sensible-utils/test_sensible-editor.sh
@@ -3,6 +3,22 @@
 
 rootfs="$(install-slices sensible-utils_sensible-editor)"
 
-chroot "$rootfs" sensible-editor --help 2>&1
+mkdir -p "$rootfs/dev" && touch "$rootfs/dev/null"
 
-exit 99 # debug stop
+chroot "$rootfs" sensible-editor 2>&1 | grep -iq "couldn't find an editor"
+
+# if nothing is there, it should default to editor
+touch "$rootfs/usr/bin/editor" && chmod +x "$rootfs/usr/bin/editor"
+chroot "$rootfs" sensible-editor
+
+# we can select the editor through the EDITOR variable
+cat <<'EOF' > "$rootfs/usr/bin/fake-editor"
+#!/bin/sh
+printf "fake-editor called with: %s\n" "$*" > /var/log/fake-editor.log
+EOF
+mkdir -p "$rootfs/var/log"
+chmod +x "$rootfs/usr/bin/fake-editor"
+
+EDITOR=/usr/bin/fake-editor chroot "$rootfs" sensible-editor
+test -f "$rootfs/var/log/fake-editor.log"
+grep -q "fake-editor called with: " "$rootfs/var/log/fake-editor.log"
--- tests/spread/integration/sensible-utils/test_sensible-pager.sh
+++ tests/spread/integration/sensible-utils/test_sensible-pager.sh
@@ -3,6 +3,22 @@
 
 rootfs="$(install-slices sensible-utils_sensible-pager)"
 
-chroot "$rootfs" sensible-pager --help 2>&1
+mkdir -p "$rootfs/dev" && touch "$rootfs/dev/null"
 
-exit 99 # debug stop
+chroot "$rootfs" sensible-pager 2>&1 | grep -iq "couldn't find a pager"
+
+# if nothing is there, it should default to pager
+touch "$rootfs/usr/bin/pager" && chmod +x "$rootfs/usr/bin/pager"
+chroot "$rootfs" sensible-pager
+
+# we can select the pager through the PAGER variable
+cat <<'EOF' > "$rootfs/usr/bin/fake-pager"
+#!/bin/sh
+printf "fake-pager called with: %s\n" "$*" > /var/log/fake-pager.log
+EOF
+mkdir -p "$rootfs/var/log"
+chmod +x "$rootfs/usr/bin/fake-pager"
+
+PAGER=/usr/bin/fake-pager chroot "$rootfs" sensible-pager
+test -f "$rootfs/var/log/fake-pager.log"
+grep -q "fake-pager called with: " "$rootfs/var/log/fake-pager.log"
