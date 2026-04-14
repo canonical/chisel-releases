c67eb2ecdca7b8a6baf651c869974df16e5b1c35
lczyk <marcin.konowalczyk@canonical.com>
2026-04-14 18:06:07 +0100

    fix: top test

--- tests/spread/integration/procps/test_top.sh
+++ tests/spread/integration/procps/test_top.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# spellchecker: ignore rootfs procps
+# spellchecker: ignore rootfs procps terminfo
 
 rootfs="$(install-slices procps_top)"
 
@@ -9,3 +9,12 @@ trap "umount '$rootfs'/proc" EXIT
 
 chroot "$rootfs" top --help 2>&1 | grep -Fiq 'usage:'
 chroot "$rootfs" top --version 2>&1 | grep -Fq 'top from procps-ng'
+
+# Run top in batch mode with bash and terminfo available
+rootfs_2="$(install-slices procps_top bash_bins)"
+
+mkdir -p "$rootfs_2"/proc
+mount --bind /proc "$rootfs_2"/proc
+trap "umount '$rootfs_2'/proc; umount '$rootfs'/proc" EXIT
+
+chroot "$rootfs_2" top -bn1 2>&1 | grep -Fq 'PID'
