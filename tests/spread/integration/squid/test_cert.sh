6654d11a9c9cae7137a89c0bb6d8544768967183
Alex Santisteban <alex.santisteban@canonical.com>
2026-04-16 17:20:47 +0200

    test: add test for cert_tool

---
+++ tests/spread/integration/squid/test_cert.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+# spellchecker: ignore rootfs
+rootfs="$(install-slices squid_cert base-files_base)"
+
+# Setup
+mount --rbind /dev "${rootfs}/dev"
+mknod -m 666 "$rootfs/dev/random" c 1 8
+mknod -m 666 "$rootfs/dev/urandom" c 1 9
+cp /etc/resolv.conf "${rootfs}/etc/resolv.conf"
+
+# Test cert_tool
+chroot "$rootfs" /usr/lib/squid/cert_tool ubuntu.com 443
+
+# Check that NSS DB files exist
+test -f "$rootfs/cert9.db"
+test -f "$rootfs/key4.db"
