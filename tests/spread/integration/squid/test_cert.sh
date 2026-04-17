055766eeab3b7779a21996c57938a139d577a7e0
Alex Santisteban <alex.santisteban@canonical.com>
2026-04-17 09:46:01 +0200

    fix: avoid failure if /dev/random and /dev/urandom already exist

--- tests/spread/integration/squid/test_cert.sh
+++ tests/spread/integration/squid/test_cert.sh
@@ -5,8 +5,8 @@ rootfs="$(install-slices squid_cert base-files_base)"
 # Setup
 mkdir -p "${rootfs}/dev"
 mount --rbind /dev "${rootfs}/dev"
-mknod -m 666 "$rootfs/dev/random" c 1 8
-mknod -m 666 "$rootfs/dev/urandom" c 1 9
+mknod -m 666 "$rootfs/dev/random" c 1 8 || true
+mknod -m 666 "$rootfs/dev/urandom" c 1 9 || true
 cp /etc/resolv.conf "${rootfs}/etc/resolv.conf"
 
 # Test cert_tool
