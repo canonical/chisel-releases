d478b3b658db25422be0b815500706984d7085b8
lczyk <marcin.konowalczyk@canonical.com>
2026-04-22 17:00:19 +0100

    chore: n=0

--- tests/spread/integration/gettext-base/test_scripts.sh
+++ tests/spread/integration/gettext-base/test_scripts.sh
@@ -8,5 +8,5 @@ test -x "$rootfs/usr/bin/gettext.sh"
 # gettext.sh can be sourced and provides eval_gettext and eval_ngettext
 c='. /usr/bin/gettext.sh && eval_gettext "Hello World"'
 chroot "$rootfs" sh -c "$c" | grep -q "Hello World"
-c='. /usr/bin/gettext.sh && n=1 && eval_ngettext "one item" "many items" 1'
+c='. /usr/bin/gettext.sh && eval_ngettext "one item" "many items" 1'
 chroot "$rootfs" sh -c "$c" | grep -q "one item"
