5629244bba40e36b05423261a05f2f35ba88c66a
Ruan Comelli <ruan.comelli@canonical.com>
2026-06-26 16:24:05 -0300

    feat(26.04): add `rust-miri` and `rust-1.93-miri`

---
+++ tests/spread/integration/rust-1.93-miri/test_run.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+# spellchecker: ignore rootfs miri resolv
+
+rootfs="$(install-slices rust-1.93-miri_cargo-miri cargo-1.93_cargo ca-certificates_data)"
+
+# Create symlinks so cargo can find the subcommand
+ln -s cargo-1.93 "$rootfs/usr/bin/cargo"
+ln -s cargo-1.93-unstable-miri "$rootfs/usr/bin/cargo-unstable-miri"
+
+mkdir -p "$rootfs/dev"
+touch "$rootfs/dev/null"
+chmod +x "$rootfs/dev/null"
+
+# We need DNS to fetch crates.io dependencies for Miri's sysroot build
+mkdir -p "$rootfs/etc" && cp /etc/resolv.conf "$rootfs/etc/resolv.conf"
+
+mkdir -p "$rootfs/proc"
+mount --bind /proc "$rootfs/proc"
+# shellcheck disable=SC2064
+trap "umount '$rootfs/proc'" EXIT
+
+# Create a simple project and run it under Miri
+chroot "$rootfs" cargo-1.93 new /hello_miri --bin --vcs none
+chroot "$rootfs" /bin/sh -c 'cd /hello_miri && cargo-1.93-unstable-miri run' |
+  grep -Fq "Hello, world!"
---
+++ tests/spread/integration/rust-miri/test_run.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+# spellchecker: ignore rootfs miri resolv
+
+rootfs="$(install-slices rust-miri_cargo-miri cargo_cargo ca-certificates_data)"
+
+mkdir -p "$rootfs/dev"
+touch "$rootfs/dev/null"
+chmod +x "$rootfs/dev/null"
+
+# We need DNS to fetch crates.io dependencies for Miri's sysroot build
+mkdir -p "$rootfs/etc" && cp /etc/resolv.conf "$rootfs/etc/resolv.conf"
+
+mkdir -p "$rootfs/proc"
+mount --bind /proc "$rootfs/proc"
+# shellcheck disable=SC2064
+trap "umount '$rootfs/proc'" EXIT
+
+# Create a simple project and run it under Miri
+chroot "$rootfs" cargo new /hello_miri --bin --vcs none
+chroot "$rootfs" /bin/sh -c 'cd /hello_miri && cargo-unstable-miri run' 2>&1 |
+  grep -Fq "Hello, world!"
