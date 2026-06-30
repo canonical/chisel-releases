03e96fb63f459951c81957e53d3dd44ab2e88ae3
Ruan Comelli <ruan.comelli@canonical.com>
2026-06-29 22:50:26 -0300

    test(26.04): add `-F` and `-i` flags to all relevant calls to `grep` in rust-miri tests

--- tests/spread/integration/rust-1.93-miri/test_help_and_version.sh
+++ tests/spread/integration/rust-1.93-miri/test_help_and_version.sh
@@ -4,12 +4,12 @@
 # test miri slice
 rootfs="$(install-slices rust-1.93-miri_miri)"
 # miri is a thin wrapper around rustc, so --version prints the rustc version
-RUSTC_BOOTSTRAP=1 chroot "$rootfs" /usr/lib/rust-1.93/bin/miri --version | grep -q 'rustc 1.93'
+RUSTC_BOOTSTRAP=1 chroot "$rootfs" /usr/lib/rust-1.93/bin/miri --version | grep -Fiq 'rustc 1.93'
 
 # test cargo-miri slice
 rootfs="$(install-slices rust-1.93-miri_cargo-miri)"
 # miri binary is pulled in via cargo-miri deps
-RUSTC_BOOTSTRAP=1 chroot "$rootfs" /usr/lib/rust-1.93/bin/miri --version | grep -q 'rustc 1.93'
+RUSTC_BOOTSTRAP=1 chroot "$rootfs" /usr/lib/rust-1.93/bin/miri --version | grep -Fiq 'rustc 1.93'
 # the wrapper sets RUSTC_BOOTSTRAP=1 itself and prints the actual miri version
-chroot "$rootfs" /usr/bin/cargo-1.93-unstable-miri --version | grep -q 'miri'
-chroot "$rootfs" /usr/bin/cargo-1.93-unstable-miri --help | grep -q -i 'miri'
+chroot "$rootfs" /usr/bin/cargo-1.93-unstable-miri --version | grep -Fiq 'miri'
+chroot "$rootfs" /usr/bin/cargo-1.93-unstable-miri --help | grep -Fiq 'miri'
