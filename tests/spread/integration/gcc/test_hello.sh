073f032afcc9594f1207450a483648fa425a87e3
lczyk <marcin.konow@lczyk.xyz>
2026-07-16 20:46:52 +0200

    fix(gcc): map ppc64le to powerpc64le triplet in hello test
    uname -m reports ppc64le but slice dir uses debian multiarch name
    powerpc64le-linux-gnu.

--- tests/spread/integration/gcc/test_hello.sh
+++ tests/spread/integration/gcc/test_hello.sh
@@ -2,6 +2,7 @@ rootfs="$(install-slices gcc_gcc libc6-dev_libs)"
 
 arch=$(uname -m)
 arch="${arch//_/-}"
+[ "${arch}" = "ppc64le" ] && arch="powerpc64le"
 arch_triplet="${arch}-linux-gnu"
 
 cp ../gcc-15-${arch_triplet}/testfiles/hello.c "${rootfs}/hello.c"
