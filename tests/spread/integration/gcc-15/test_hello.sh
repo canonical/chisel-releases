d0ac476ddb1a20e3bf5137df4c721109068c5687
Marcin Konowalczyk <marcin.konowalczyk@canonical.com>
2026-07-16 12:09:34 +0200

    fix: readd arch triplet
    Co-authored-by: zhijie-yang <zhijie.yang@canonical.com>

--- tests/spread/integration/gcc-15/test_hello.sh
+++ tests/spread/integration/gcc-15/test_hello.sh
@@ -1,5 +1,9 @@
 rootfs="$(install-slices gcc-15_gcc-15 libc6-dev_libs)"
 
+arch=$(uname -m)
+arch="${arch//_/-}"
+arch_triplet="${arch}-linux-gnu"
+
 cp ../gcc-15-${arch_triplet}/testfiles/hello.c "${rootfs}/hello.c"
 
 chroot "${rootfs}" gcc-15 -o hello hello.c
