c8e46f2c370a0ffe947fa337a50f2ec4f6771c79
Alex Santisteban <alex.santisteban@canonical.com>
2026-07-27 09:56:10 +0200

    feat(22.04): add cpp slices

---
+++ tests/spread/integration/cpp-11/test_preprocessor.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+# spellchecker: ignore rootfs libexec ifdef ifndef
+
+arch=$(uname -m)
+case "$arch" in
+    x86_64)   triplet="x86_64-linux-gnu" ;;
+    aarch64)  triplet="aarch64-linux-gnu" ;;
+    ppc64le)  triplet="powerpc64le-linux-gnu" ;;
+    s390x)    triplet="s390x-linux-gnu" ;;
+    *)
+        echo "Unsupported architecture: $arch"
+        exit 1
+        ;;
+esac
+
+rootfs="$(install-slices \
+    base-files_bin \
+    cpp-11_cc1 \
+)"
+
+ln -s "/usr/lib/gcc/${triplet}/11/cc1" "${rootfs}/usr/bin/cc1"
+
+cp question.c "${rootfs}/question.c"
+
+# no answer, therefore default answer
+chroot "${rootfs}" cc1 -E question.c > "${rootfs}/question.i" 2>/dev/null
+cat "${rootfs}/question.i" | grep -q 'return 1;'
+
+# specify ANSWER
+chroot "${rootfs}" cc1 -DANSWER=42 -E question.c > "${rootfs}/question.i" 2>/dev/null
+cat "${rootfs}/question.i" | grep -q 'return 42;'
